bbc8ba405a9fe3366e4249a1db01f8309e75a8e0
ZhenyuHu <cszhenyuhu@gmail.com>
2017-04-17 15:09:51 +0800

    (Fix): fix the schema type
    Overview: fix the schema type
    
    Detailed Description:
     	1. the field error  (emial --> email)
    	2. to support email verify add the field <verify>,and the default value is false!
    
    Build: Pass
    FunctionalTest: Pass
    PerformanceTest: N/A

--- public/javascripts/accountSchema.js
+++ public/javascripts/accountSchema.js
@@ -6,8 +6,9 @@ var Schema = mongoose.Schema;
 var AccountSchema = new Schema({
     account: { type: String },
     password: { type: String },
-    emial: { type: String },
-    phonenumber: { type: String }
+    email: { type: String },
+    phonenumber: { type: String },
+    verify:{type: Boolean,default:false}
 });
 
 module.exports = connectAccount.model('Account',AccountSchema);
